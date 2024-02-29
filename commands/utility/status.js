c1aa6d4c812141339477c9625eb45624f6657945
kayqb <ferocity2max@gmail.com>
2024-02-29 19:03:50 +0000

    add cooldown and register events

--- commands/utility/status.js
+++ commands/utility/status.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
+	cooldown: 10,
 	data: new SlashCommandBuilder()
 		.setName('status')
 		.setDescription('Check the current status of the bot.!'),
